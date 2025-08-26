d7f49d03889725c6cd692597a6bb45be7acae7d4
Juan Pablo <jpriveram01@gmail.com>
2025-08-26 10:10:29 -0400

    feat: Convierte un num que no es multiplo de 3 o 5

---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,8 @@
+ import convertirFizzBuzz from "./FizzBuzz.js";
+
+describe("FizzBuzz", () => {
+  it("deberia convertir un numero que no es multiplo de 3 ni 5 ", () => {
+    expect(convertirFizzBuzz(1)).toEqual("1");
+  });
+});
+
